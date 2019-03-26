578ac7ceaecc517056296c2a7c7177f350b51005
mvsw2 <335146335@qq.com>
2019-03-26 16:56:11 +0800

    Initial commit

---
+++ openlib/src/main/java/com/mv/openlib/Hello.java
@@ -0,0 +1,16 @@
+package com.mv.openlib;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2019/3/26.
+ */
+
+public class Hello {
+    public void sayHello(){
+        Log.e("libCall","openlib call:sayHello");
+    }
+    public static void sayHello2(){
+        Log.e("libCall","openlib call:sayHello2");
+    }
+}
